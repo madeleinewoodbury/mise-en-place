a3f9f55723659165b574d4edb8c718f9cce0dbba
Madeleine Woodbury <randine25@gmail.com>
2019-04-23 13:45:33 -0400

    Bootstrap and Font Awesome implemented. Navbar markup and styling implemented

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,9 +3,40 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav id="main-nav" className="navbar navbar-expand-lg navbar-light py-2">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            <i className="fas fa-utensils fa-2x align-middle" />
+            <h3 className="d-inline align-middle pl-2">Mise en Place</h3>
+          </a>
+          <button
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="recipes.html" className="nav-link">
+                  Recipes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="login.html" className="nav-link">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="register.html" className="nav-link">
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
