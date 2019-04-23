312e38b854bc8bdd259ecf2276abfe41b224d260
Madeleine Woodbury <randine25@gmail.com>
2019-04-23 14:23:49 -0400

    Login and Register components markup and styling added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,8 +3,39 @@ import React, { Component } from "react";
 class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div id="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto py-3">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your Mise en Place account
+              </p>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,55 @@ import React, { Component } from "react";
 class Register extends Component {
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+      <div id="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto py-3">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your Mise en Place account
+              </p>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
