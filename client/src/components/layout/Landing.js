860ed18313e1d68949bea5d1ecf800d702d2fee8
Madeleine Woodbury <randine25@gmail.com>
2019-04-23 14:03:16 -0400

    Landing component markup and styling

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,8 +3,28 @@ import React, { Component } from "react";
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing</h1>
+      <div id="landing" className="py-5">
+        <div className="primary-overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 text-center text-white">
+                <h1 className="display-5 mt-4 pt-4">
+                  "A recipe is a story that ends with a good meal"
+                </h1>
+                <h3>
+                  <em>- Pat Conroy</em>
+                </h3>
+                <p className="lead mt-3 p-2">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.{" "}
+                  <br />
+                  <a href="/" className="btn btn-primary btn-lg mt-3">
+                    View Recipes
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
